e9c326d49484664863b51b16feeed76a88d2e111
Aluno Senac <Aluno@EDU72811LAB2PC.edu.sesc-ce.com.br>
2025-05-14 12:00:13 -0300

    Finished Area_1 and started the cutscene WIP

--- exit_door.gd
+++ exit_door.gd
@@ -4,15 +4,15 @@ var _can_exit = false
 
 func _physics_process(delta: float) -> void:
 	if _can_exit:
+		print("Can exit")
 		if Input.is_action_just_pressed("Enter"):
 			get_tree().change_scene_to_file("res://map_1_5.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "PlayerHouse":
 		_can_exit = true
-		print("Can exit")
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "PlayerHouse":
 		_can_exit = false
--- house_door.gd
+++ house_door.gd
@@ -3,12 +3,19 @@ extends Area2D
 var _can_enter = false
 
 func _physics_process(delta: float) -> void:
-	if _can_enter and Input.is_action_just_pressed("Enter"):
-		get_tree().change_scene_to_file("res://House.tscn")
+	if _can_enter:
+		print("Pode entrar")
+		if _can_enter and Input.is_action_just_pressed("Enter"):
+			_can_enter = false
+			get_tree().change_scene_to_file("res://House.tscn")
 		
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		_can_enter = true
-		print("Pode entrar")
 		
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		_can_enter = false
